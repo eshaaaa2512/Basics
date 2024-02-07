676f92e434cc5174fd16f25c0b0f654efe78cf06
Esha <eshadeswal@gmail.com>
2024-02-07 12:26:01 +0530

    [Esha] Add: Swapping the numbers

--- pg1.cs
+++ pg1.cs
@@ -10,20 +10,25 @@ namespace Basic_program
     {
         static void Main(string[] args)
         {
-            int a;
-            Console.WriteLine("Enter the number:-");
+            int a, b, c;
+            Console.WriteLine("Enter the first number:-");
             a = Convert.ToInt32(Console.ReadLine());
-            if (a >= 0)
-            {
-                Console.WriteLine("Number is positive");
-            }
-            else
-            {
-                Console.WriteLine("Number is Negative");
-            }
+            Console.WriteLine("Enter the Second number:-");
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("After swapping a will be :-" + (a));
+            Console.WriteLine("After swapping b will be :-" + (b));
             Console.ReadLine();
         }
     }
+}
+
+        
+       
+
+    
 }
         
     
