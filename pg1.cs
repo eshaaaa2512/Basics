c129cfbb5757cec7616b4c0738d38edd740fead7
Esha <eshadeswal@gmail.com>
2024-02-07 11:54:28 +0530

    [Esha] Add: positive and negative

--- pg1.cs
+++ pg1.cs
@@ -10,18 +10,21 @@ namespace Basic_program
     {
         static void Main(string[] args)
         {
-            int i;
+            int a;
             Console.WriteLine("Enter the number:-");
-            i= Convert.ToInt32(Console.ReadLine());
-            if (i % 2 == 0)
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a >= 0)
             {
-                Console.WriteLine("The number is even");
+                Console.WriteLine("Number is positive");
             }
             else
             {
-                Console.WriteLine("The number is odd");
+                Console.WriteLine("Number is Negative");
             }
             Console.ReadLine();
         }
     }
 }
+        
+    
+
