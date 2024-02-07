df81b3850e6587ac9241f9f7862cb2baf029b38b
Esha <eshadeswal@gmail.com>
2024-02-07 11:13:34 +0530

    First commit

---
+++ pg1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("Enter the number:-");
+            i= Convert.ToInt32(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("The number is even");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd");
+            }
+            Console.ReadLine();
+        }
+    }
+}
