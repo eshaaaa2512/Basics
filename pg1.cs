8a3648c9773e9641e3f8c15a6a5765d57dd7c21a
Esha <eshadeswal@gmail.com>
2024-02-08 15:38:29 +0530

    [Esha]Add: did all the methods add div and mul

--- pg1.cs
+++ pg1.cs
@@ -10,18 +10,26 @@ namespace Basic_program
     {
         static void Main(string[] args)
         {
-            int a;
-            Console.WriteLine("Enter the number:-");
-            a = Convert.ToInt32(Console.ReadLine());
-            if (a % 2 == 0)
-            {
-                Console.WriteLine("The number is divisible by 2");
-            }
-            else
-            {
-                Console.WriteLine("The number is not divible by 2");
-            }
+            int a, b;
+            Console.WriteLine("Enter the first number:-");
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number-");
+            b=Convert.ToInt32(Console.ReadLine());
+            
+            int sum = a + b;
+
+
+
+            
+            int mul = a * b;
+            
+            float div = a / b;
+            Console.WriteLine("Sum of the number:" + (sum));
+            Console.WriteLine("The multiplication of the number is:"  + (mul));
+            Console.WriteLine("The division of the number is:" + (div));
             Console.ReadLine();
+            
+
         }
     }
 }
