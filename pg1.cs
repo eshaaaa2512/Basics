5dfe7788b7cfc6cdf2f51c8216dd2641840bb591
Esha <eshadeswal@gmail.com>
2024-02-10 10:32:11 +0530

    [Esha]Add: the number comes under 3 nd 5

--- pg1.cs
+++ pg1.cs
@@ -1,39 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Basic_program
+
+class program
 {
-    internal class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        int a, b, i, Sum = 0;
+        for (i = 1; i < 100; i++)
         {
-            int a;
-            Console.WriteLine("Enter the number:-");
-            a = Convert.ToInt32(Console.ReadLine());
-            if (a % 2 == 0)
+            a = i % 3;
+            b = i % 5;
+            if (a == 0 || b == 0)
             {
-                Console.WriteLine("The number is divisible by 2");
+                Console.Write("{0}", i);
+                Sum = Sum + i;
             }
-            else
-            {
-                Console.WriteLine("The number is not divible by 2");
-            }
-            Console.ReadLine();
         }
+        Console.WriteLine("\nThe Sum of all the Multiples of 3 or 5 Below 100 : {0}",Sum);
+        Console.Read();
     }
 }
 
 
-        
 
 
 
-    
 
 
-        
-    
+
+
+
+
 
